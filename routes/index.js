6b10048f26e5f853ca404bc56cbe4a24fa1e4d9d
vigueras58 <ant.vigueras@gmail.com>
2015-07-10 20:42:45 +0200

    version 3
    pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
+var quizController = require('../controllers/quizController');
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+res.render('index', { title: 'Quiz'});
 });
 
+router.get('quizes/question',quizController.question);
+router.get('quizes/answer',quizController.answer);
 module.exports = router;
